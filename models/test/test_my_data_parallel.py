584f3a0f76f2b02b4c2e1d720f1ec03727031abe
Gunnar Sigurdsson <gunnars@allenai.org>
2018-11-16 16:55:24 -0500

    debug dataparallel unittests

--- models/test/test_my_data_parallel.py
+++ models/test/test_my_data_parallel.py
@@ -9,18 +9,21 @@ class TestMyDataParallel(unittest.TestCase):
             data = torch.Tensor(10, 2, 3)
             my_data_parallel = MyDataParallel(torch.nn.Module())
             out, _ = my_data_parallel.scatter((data, ), {}, [0, 1])
-            # out is #gpu x #args # tensor_dims
+            # out is #gpu x #args x tensor_dims
             self.assertEqual(len(out), 2)
-            import pdb
-            pdb.set_trace()
             self.assertSequenceEqual(out[0][0].shape, (5, 2, 3))
 
-    #def test_scatter_do_not_collate(self):
-    #    if torch.cuda.is_available():
-    #        inp = torch.Tensor(10, 2, 3)
-    #        meta = {'data': torch.Tensor(3, 4)}
-    #        data = (inp, meta)
-    #        my_data_parallel = MyDataParallel(torch.nn.Module())
-    #        out = my_data_parallel.scatter(data, {}, [0, 1])
-    #        self.assertEqual(len(out), 2)
-    #        self.assertSequenceEqual(out[0].shape, (5, 2, 3))
+    def test_scatter_do_not_collate(self):
+        if torch.cuda.is_available():
+            inp = torch.Tensor(10, 2, 3)
+            meta = [{'do_not_collate': True,
+                    'data': torch.Tensor(3, 4)}] * 10
+            data = (inp, meta)
+            my_data_parallel = MyDataParallel(torch.nn.Module())
+            out, _ = my_data_parallel.scatter(data, {}, [0, 1])
+            import pdb
+            pdb.set_trace()
+            self.assertEqual(len(out), 2)
+            self.assertSequenceEqual(out[0][0].shape, (5, 2, 3))
+            self.assertEqual(len(out[0][1]), 5)
+            self.assertSequenceEqual(out[0][1]['data'].shape, (3, 4))

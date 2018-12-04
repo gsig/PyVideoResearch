be2b404cd5efe4d4373ca970b9ae9445350bd1f3
Gunnar Sigurdsson <gunnars@allenai.org>
2018-12-03 21:45:29 -0500

    initial third to first actor observer

--- checkpoints.py
+++ checkpoints.py
@@ -30,6 +30,9 @@ def load_partial_state(model, state_dict):
         k = k0.replace('module.', '').replace('mA.', '').replace('basenet.', '')
         if k not in sd or not sd[k].shape == v.shape:
             print('ignoring state key for loading: {}'.format(k))
+            import pdb
+            pdb.set_trace()
+            print('asdf')
             continue
         if isinstance(v, torch.nn.Parameter):
             v = v.data

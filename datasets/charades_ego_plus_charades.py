24ae0e29a54e0cebd305bea5703cda309dff23ec
Gunnar Sigurdsson <gunnars@allenai.org>
2018-12-05 20:17:17 -0500

    actor observer classification

--- datasets/charades_ego_plus_charades.py
+++ datasets/charades_ego_plus_charades.py
@@ -26,7 +26,7 @@ class CharadesMeta(Charades):
         return ims, target, meta
 
 
-class CharadesEgoPlusCharades(CharadesMeta):
+class CharadesEgoPlusCharades(Charades):
     @classmethod
     def get(cls, args):
         newargs = copy.deepcopy(args)
@@ -42,25 +42,11 @@ class CharadesEgoPlusCharades(CharadesMeta):
         train_datasetego, val_datasetego, _ = CharadesEgo.get(args)
         train_dataset, val_dataset, valvideo_dataset = super(CharadesEgoPlusCharades, cls).get(newargs)
 
-        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                         std=[0.229, 0.224, 0.225])
         train_dataset.transform.transforms.append(transforms.Lambda(lambda x: [x, x, x]))
         val_dataset.transform.transforms.append(transforms.Lambda(lambda x: [x, x, x]))
         valvideo_dataset.transform.transforms.append(transforms.Lambda(lambda x: [x, x, x]))
         train_dataset.target_transform = transforms.Lambda(lambda x: -x)
         val_dataset.target_transform = transforms.Lambda(lambda x: -x)
-
-        valvideoego_dataset = CharadesMeta(
-            args.data, 'val_video',
-            args.egocentric_test_data,
-            args.cache,
-            args.cache_buster,
-            transform=transforms.Compose([
-                transforms.Resize(int(256. / 224 * args.inputsize)),
-                transforms.CenterCrop(args.inputsize),
-                transforms.ToTensor(),
-                normalize,
-            ]))
 
         train_dataset = ConcatDataset([train_dataset] + [train_datasetego] * 6)
         val_dataset = ConcatDataset([val_dataset] + [val_datasetego] * 6)

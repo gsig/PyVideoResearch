ee7c8188e243cec10b3f3fb5c0eb3ce6932ef2f8
Gunnar Sigurdsson <gunnars@allenai.org>
2018-12-05 20:03:21 -0500

    actor observer classification

--- exp/class321a.py
+++ exp/class321a.py
@@ -13,7 +13,7 @@ name = __file__.split('/')[-1].split('.')[0]  # name is filename
 
 args = [
     '--name', name,
-    '--dataset', 'charades_ego',
+    '--dataset', 'charades_ego_plus_charades',
     '--print-freq', '1',
     '--arch', 'resnet152',
     '--wrapper', 'actor_observer_with_classifier_wrapper',

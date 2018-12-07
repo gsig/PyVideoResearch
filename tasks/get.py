a19acbcecdf68689ae0afbd60efa3d468c5fb937
Gunnar Sigurdsson <gunnars@allenai.org>
2018-12-07 13:11:37 -0500

    actor observer 3d with classifier

--- tasks/get.py
+++ tasks/get.py
@@ -3,6 +3,8 @@ from importlib import import_module
 
 
 def get_tasks(tasks):
+    if tasks == '':
+        return []
     if type(tasks) == str:
         tasks = tasks.split(';')
     return [case_getattr(import_module('tasks.' + m), m) for m in tasks]

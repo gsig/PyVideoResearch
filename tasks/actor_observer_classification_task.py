e70ed9bcd5134ab91dfc93869f48e81f25a074f5
Gunnar Sigurdsson <gunnars@allenai.org>
2018-12-07 23:59:21 -0500

    only 1st/3rd classification tasks

--- tasks/actor_observer_classification_task.py
+++ tasks/actor_observer_classification_task.py
@@ -1,6 +1,7 @@
 from datasets.get import get_dataset
 from tasks.video_task import VideoTask
 from models.wrappers.actor_observer_classifier_wrapper import ActorObserverClassifierWrapper
+from models.utils import set_distributed_backend
 
 
 class ActorObserverClassificationTask(VideoTask):
@@ -10,6 +11,7 @@ class ActorObserverClassificationTask(VideoTask):
     @classmethod
     def run(cls, model, criterion, epoch, args):
         model = ActorObserverClassifierWrapper(model, args)
+        model = set_distributed_backend(model, args)
         task = cls(model, epoch, args)
         loader, = get_dataset(args, splits=('val_video', ), dataset=args.actor_observer_classification_task_dataset)
         return task.validate_video(loader, model, criterion, epoch, args)

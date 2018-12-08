638a81e7bb3d34b33c188820c89ecd63700fa896
Gunnar Sigurdsson <gunnars@allenai.org>
2018-12-08 00:14:28 -0500

    only 1st/3rd classification tasks

--- tasks/actor_observer_classification_task.py
+++ tasks/actor_observer_classification_task.py
@@ -2,7 +2,7 @@ from datasets.get import get_dataset
 from tasks.video_task import VideoTask
 from models.wrappers.actor_observer_classifier_wrapper import ActorObserverClassifierWrapper
 from models.utils import set_distributed_backend
-import torch.nn as nn
+from models.criteria.default_criterion import DefaultCriterion
 
 
 class ActorObserverClassificationTask(VideoTask):
@@ -13,7 +13,7 @@ class ActorObserverClassificationTask(VideoTask):
     def run(cls, model, criterion, epoch, args):
         model = ActorObserverClassifierWrapper(model, args)
         model = set_distributed_backend(model, args)
-        criterion = nn.BCELoss()
+        criterion = DefaultCriterion(args)
         task = cls(model, epoch, args)
         loader, = get_dataset(args, splits=('val_video', ), dataset=args.actor_observer_classification_task_dataset)
         return task.validate_video(loader, model, criterion, epoch, args)

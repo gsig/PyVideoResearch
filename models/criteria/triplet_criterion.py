bd64d7e0ef80f1e25f4d376225eb3db25baabf25
Gunnar Sigurdsson <gunnars@allenai.org>
2018-12-10 19:45:53 -0500

    actor observer improve 321

--- models/criteria/triplet_criterion.py
+++ models/criteria/triplet_criterion.py
@@ -9,12 +9,13 @@ class TripletCriterion(Criterion):
         self.margin = args.margin
 
     def forward(self, dist_a, dist_b, x, y, z, target, meta, synchronous=False):
-        ids = meta['id']
         w = x * y * z
         loss = self.loss.apply(dist_a, dist_b, target, self.margin)
+        final = loss.sum()
         print('loss before', loss.sum().item())
 
         pred = {'triplet_prediction': [(a, b) for a, b in zip(dist_a.detach().cpu(), dist_b.detach().cpu())],
                 'weights': w.detach().cpu()}
         targ = {'triplet_target': target.cpu()}
-        return pred, loss, targ
+        return pred, final, targ
+

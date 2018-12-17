bd4032e14bdd76956c409d3cef21b5fc1809d4d9
Gunnar Sigurdsson <gunnars@allenai.org>
2018-12-17 14:32:57 -0500

    debugging actor observer validation

--- exp/321a5_debug8.py
+++ exp/321a5_debug8.py
@@ -13,7 +13,7 @@ name = __file__.split('/')[-1].split('.')[0]  # name is filename
 
 args = [
     '--name', name,
-    '--dataset', 'charades_ego2',
+    '--dataset', 'charades_ego',
     '--print-freq', '1',
     '--arch', 'resnet50',
     '--wrapper', 'actor_observer_wrapper',

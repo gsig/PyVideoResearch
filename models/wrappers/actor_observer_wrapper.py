3d7e61229593f066f9a84c1241f9169ee3390492
Gunnar Sigurdsson <gunnars@allenai.org>
2018-12-17 19:12:43 -0500

    debugging actor observer adding features back

--- models/wrappers/actor_observer_wrapper.py
+++ models/wrappers/actor_observer_wrapper.py
@@ -20,14 +20,12 @@ class ActorObserverWrapper(DefaultWrapper):
         self.third_fc = nn.Sequential(nn.Linear(dim, 1), nn.Tanh())
         self.third_scale = nn.Parameter(torch.Tensor([math.log(.5)]))
         self.distance = opts.distance
-        #if opts.share_selector:
-        #    self.firstneg_fc = self.firstpos_fc
-        #    self.firstneg_scale = self.firstpos_scale
-        #else:
-        #    self.firstneg_fc = nn.Sequential(nn.Linear(dim, 1), nn.Tanh())
-        #    self.firstneg_scale = nn.Parameter(torch.Tensor([math.log(.5)]))
-        self.firstneg_fc = nn.Sequential(nn.Linear(dim, 1), nn.Tanh())
-        self.firstneg_scale = nn.Parameter(torch.Tensor([math.log(.5)]))
+        if opts.share_selector:
+            self.firstneg_fc = self.firstpos_fc
+            self.firstneg_scale = self.firstpos_scale
+        else:
+            self.firstneg_fc = nn.Sequential(nn.Linear(dim, 1), nn.Tanh())
+            self.firstneg_scale = nn.Parameter(torch.Tensor([math.log(.5)]))
 
     def base(self, x, y, z):
         #base_y = self.basenet(y)
@@ -41,16 +39,14 @@ class ActorObserverWrapper(DefaultWrapper):
         base_y = self.basenet(y)
         base_z = self.basenet(z)
 
-        dist_a = F.pairwise_distance(base_x, base_y, 2).view(-1)
-        dist_b = F.pairwise_distance(base_y, base_z, 2).view(-1)
-        #if self.distance == 'cosine':
-        #    dist_a = .5 - .5 * F.cosine_similarity(base_x, base_y, 1, 1e-6).view(-1)
-        #    dist_b = .5 - .5 * F.cosine_similarity(base_y, base_z, 1, 1e-6).view(-1)
-        #elif self.distance == 'l2':
-        #    dist_a = F.pairwise_distance(base_x, base_y, 2).view(-1)
-        #    dist_b = F.pairwise_distance(base_y, base_z, 2).view(-1)
-        #else:
-        #    assert False, "Wrong args.distance"
+        if self.distance == 'cosine':
+            dist_a = .5 - .5 * F.cosine_similarity(base_x, base_y, 1, 1e-6).view(-1)
+            dist_b = .5 - .5 * F.cosine_similarity(base_y, base_z, 1, 1e-6).view(-1)
+        elif self.distance == 'l2':
+            dist_a = F.pairwise_distance(base_x, base_y, 2).view(-1)
+            dist_b = F.pairwise_distance(base_y, base_z, 2).view(-1)
+        else:
+            assert False, "Wrong args.distance"
 
         print('fc7 norms:', base_x.norm().item(), base_y.norm().item(), base_z.norm().item())
         print('pairwise dist means:', dist_a.mean().item(), dist_b.mean().item())
@@ -63,14 +59,14 @@ class ActorObserverWrapper(DefaultWrapper):
               math.exp(self.firstneg_scale.item()))
 
     def forward(self, inputs, meta):
-        #if self.freeze_batchnorm:
-        #    for module in self.basenet.modules():
-        #        if isinstance(module, torch.nn.modules.BatchNorm1d):
-        #            module.eval()
-        #        if isinstance(module, torch.nn.modules.BatchNorm2d):
-        #            module.eval()
-        #        if isinstance(module, torch.nn.modules.BatchNorm3d):
-        #            module.eval()
+        if self.freeze_batchnorm:
+            for module in self.basenet.modules():
+                if isinstance(module, torch.nn.modules.BatchNorm1d):
+                    module.eval()
+                if isinstance(module, torch.nn.modules.BatchNorm2d):
+                    module.eval()
+                if isinstance(module, torch.nn.modules.BatchNorm3d):
+                    module.eval()
         x, y, z = inputs
         """ assuming:
             x: first person positive

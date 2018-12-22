b77730d448179d807927ce8eb335e70c2c087c08
Gunnar Sigurdsson <gunnars@allenai.org>
2018-12-22 18:59:10 -0500

    stabilization initial commit

--- tasks/stabilization_task.py
+++ tasks/stabilization_task.py
@@ -67,14 +67,15 @@ class StabilizationTask(Task):
                 output = self.stabilize_video(inputs, model, args)
 
             # save videos
+            name = '{}_{}'.format(meta['id'][0], meta['start'][0])
             original = original[0]
             output = output[0]
             original -= torch.Tensor([0.485, 0.456, 0.406])[None, None, None, :].to(original.device)
             original -= torch.Tensor([0.229, 0.224, 0.225])[None, None, None, :].to(original.device)
             output *= torch.Tensor([0.485, 0.456, 0.406])[None, None, None, :].to(output.device)
             output *= torch.Tensor([0.229, 0.224, 0.225])[None, None, None, :].to(output.device)
-            ffmpeg_video_writer(original.cpu(), args.cache + '/original.mp4')
-            ffmpeg_video_writer(output.cpu(), args.cache + '/stabilized.mp4')
+            ffmpeg_video_writer(original.cpu(), '{}/{}_original.mp4'.format(args.cache, name))
+            ffmpeg_video_writer(output.cpu(), '{}/{}_stabilized.mp4'.format(args.cache, name))
             timer.tic()
             print('Stabilization: [{0}/{1}]\t'
                   'Time {timer.val:.3f} ({timer.avg:.3f})'.format(

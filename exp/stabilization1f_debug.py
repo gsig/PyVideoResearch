8383a90d87e825008c0f01c8675810a74fa40507
Gunnar Sigurdsson <gunnars@allenai.org>
2018-12-23 13:37:15 -0500

    stabilization transformer

--- exp/stabilization1f_debug.py
+++ exp/stabilization1f_debug.py
@@ -28,7 +28,7 @@ args = [
     '--batch-size', '1',
     '--val-size', '0',
     '--cache-dir', '/nfs.yoda/gsigurds/caches/',
-    '--epochs', '10',
+    '--epochs', '100',
     '--workers', '0',
     '--evaluate',
     '--features', 'conv1;fc',

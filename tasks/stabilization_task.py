9ac7d61b7ded1b72eff565022163626845f29a24
Gunnar Sigurdsson <gunnars@allenai.org>
2018-12-25 21:26:28 -0500

    stabilization conv1 motion

--- tasks/stabilization_task.py
+++ tasks/stabilization_task.py
@@ -146,7 +146,9 @@ class StabilizationTask(Task):
                 assert False, "invalid stabilization target"
             content_loss = F.mse_loss(output['fc'], target['fc'])
             style_loss = F.mse_loss(gram_matrix(output['layer1']), gram_matrix(target['layer1']))
-            motion_loss = F.l1_loss(video_transformed[:, 1:, :, :], video_transformed[:, :-1, :, :])
+            # motion_loss = F.l1_loss(video_transformed[:, 1:, :, :], video_transformed[:, :-1, :, :])
+            print(output['conv1'].shape)
+            motion_loss = F.l1_loss(output['conv1'][:, 1:, :, :], output['conv1'][:, :-1, :, :])
             loss = (content_loss * self.content_weight +
                     motion_loss * self.motion_weight +
                     style_loss * self.style_weight +

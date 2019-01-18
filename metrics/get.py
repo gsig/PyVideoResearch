955fc4c2612f17f6b683f9ec391a6a97da5639a3
Gunnar Sigurdsson <gunnars@allenai.org>
2019-01-18 13:59:01 +0100

    autoencoder

--- metrics/get.py
+++ metrics/get.py
@@ -3,6 +3,8 @@ from importlib import import_module
 
 
 def get_metrics(metrics):
+    if metrics == '':
+        return []
     if type(metrics) == str:
         metrics = metrics.split(';')
     return [case_getattr(import_module('metrics.' + m), m) for m in metrics]

120e5696c44ec69fc77d26c6ec5fabb7ba377051
Gunnar Sigurdsson <gunnars@allenai.org>
2019-01-20 22:33:30 +0100

    stabilization autoencoder

--- tasks/stabilization_autoencoder_task.py
+++ tasks/stabilization_autoencoder_task.py
@@ -89,7 +89,7 @@ class StabilizationAutoencoderTask(Task):
     def fine_tune_autoencoder(self, inputs, model, args):
         model = copy.deepcopy(model)
         params = model.parameters()
-        lr = 1e-5
+        lr = 0.005
         optimizer = torch.optim.SGD(params, lr=lr, momentum=args.momentum, weight_decay=0)
         criteria = AutoencoderCriterion(args)
         tol = 1e-2

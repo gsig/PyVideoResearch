03eecfab510a94be925356b9a9bd14692777cd49
Gunnar Sigurdsson <gunnars@allenai.org>
2019-01-22 13:04:16 +0100

    stabilization autoencoder

--- tasks/autoencoder_task.py
+++ tasks/autoencoder_task.py
@@ -28,12 +28,12 @@ class AutoencoderTask(Task):
         return task.stabilize_all(loader, model, epoch, args)
 
     def fine_tune_autoencoder(self, inputs, model, args):
-        model = copy.deepcopy(model)
+        #model = copy.deepcopy(model)
         params = model.parameters()
-        lr = 1e-4
+        #lr = 1e-4
         warmup = 10
-        optimizer = torch.optim.SGD(params, lr=lr, momentum=args.momentum, weight_decay=0)
-        #optimizer = torch.optim.Adam(params, lr=lr, weight_decay=0)
+        #optimizer = torch.optim.SGD(params, lr=lr, momentum=args.momentum, weight_decay=0)
+        optimizer = torch.optim.Adam(params, lr=args.lr, weight_decay=0)
         criteria = AutoencoderCriterion(args)
         tol = 1e-1
         loss = torch.Tensor([999])

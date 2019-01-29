98d34fc847c36131b107c25b8462ff5c1125c7f4
Gunnar Sigurdsson <gunnars@allenai.org>
2019-01-29 18:49:34 +0100

    sfmnet dev

---
+++ datasets/charades_video_sfm2.py
@@ -0,0 +1,27 @@
+from datasets.charades_video import CharadesVideo
+import cv2
+import torch
+import numpy as np
+
+
+class CharadesVideoSfm2(CharadesVideo):
+    def __init__(self, *args, **kwargs):
+        if 'train_gap' not in kwargs:
+            kwargs['train_gap'] = 2
+        super(CharadesVideoSfm2, self).__init__(*args, **kwargs)
+
+    def get_item(self, index, shift=None):
+        img, target, meta = super(CharadesVideoSfm2, self).forward(index, shift)
+        video = img.clone()
+        video *= torch.Tensor([0.229, 0.224, 0.225])[None, None, None, :]
+        video += torch.Tensor([0.485, 0.456, 0.406])[None, None, None, :]
+        video = np.asarray(np.clip(video*255., 0, 255), dtype="uint8")
+        prev_frame = video[0]
+        frame = video[1]
+        prev_frame_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+        current_frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        flow = cv2.calcOpticalFlowFarneback(prev_frame_gray, current_frame_gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+        if mag.mean() < 1:
+            raise ValueError('Less then 1 pixel average optical flow in the frame, skipping')
+        return img, target, meta

aa86b15d1dad1a3da1dcfb77e5e1771a1acb6433
Gunnar Sigurdsson <gunnars@allenai.org>
2019-01-30 17:49:29 +0100

    stabilization homography and augmentation

--- models/layers/video_homography_deformer.py
+++ models/layers/video_homography_deformer.py
@@ -7,7 +7,7 @@ class VideoHomographyDeformer(nn.Module):
     def __init__(self, channels):
         super(VideoHomographyDeformer, self).__init__()
         self.theta = nn.Parameter(torch.Tensor(channels, 3 * 2))
-        self.projection = nn.Parameter(torch.Tensor(channels, 3 * 2))
+        self.projection = nn.Parameter(torch.Tensor(channels, 3 * 1))
         identity = torch.Tensor([1, 0, 0, 0, 1, 0]).float()
         self.theta.data.copy_(identity[None, :].repeat(channels, 1))
         self.projection.data.zero_()
@@ -20,11 +20,9 @@ class VideoHomographyDeformer(nn.Module):
             x = x.permute(0, 3, 1, 2)
         theta = self.theta.view(-1, 2, 3)
         grid = F.affine_grid(theta, x.size())
-        projection = self.projection.view(-1, 2, 3)
-        #projection[:, 1, 0] = projection[:, 0, 0]
-        #projection[:, 1, 1] = projection[:, 0, 1]
+        projection = self.projection.view(-1, 1, 3)
         projection[:, 0, 2] = 1
-        projection[:, 1, 2] = 1
+        projection = projection.repeat(1, 2, 1)
         grid_projection = F.affine_grid(projection, x.size())
         grid = grid / grid_projection
         x = F.grid_sample(x, grid, padding_mode="reflection")

48aced88fc19f5821e5274ba22a6de2fd40631c3
Gunnar Sigurdsson <gunnars@allenai.org>
2019-01-30 14:53:11 +0100

    sfmnet dev

---
+++ exp/sfmnet1c2.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+import sys
+import os
+import subprocess
+import traceback
+import pdb
+from bdb import BdbQuit
+subprocess.Popen('find ./exp/.. -iname "*.pyc" -delete'.split())
+sys.path.insert(0, '.')
+os.nice(19)
+from main import main
+name = __file__.split('/')[-1].split('.')[0]  # name is filename
+
+args = [
+    '--name', name,
+    '--dataset', 'charades_video_sfm',
+    '--print-freq', '1',
+    '--arch', 'resnet18',
+    '--wrapper', 'sfmlearner_wrapper',
+    '--criterion', 'sfmlearner_criterion',
+    '--train-file', '/nfs.yoda/gsigurds/CharadesEgo_v1_train.csv',
+    '--val-file', '/nfs.yoda/gsigurds/CharadesEgo_v1_test.csv',
+    '--data', '/scratch/gsigurds/CharadesEgo_v1_rgb/',
+    '--resume', '/nfs.yoda/gsigurds/caches/' + name + '/model.pth.tar',
+    '--lr', '2e-4',
+    '--lr-decay-rate', '100',
+    '--optimizer', 'adam',
+    '--weight-decay', '0',
+    '--batch-size', '10',
+    '--train-size', '8.0',
+    '--val-size', '1.0',
+    '--cache-dir', '/nfs.yoda/gsigurds/caches/',
+    '--epochs', '40',
+    '--workers', '4',
+    '--photo-loss-weight', '1.0',
+    '--mask-loss-weight', '1.0',
+    '--smooth-loss-weight', '0.1',
+    '--inverse-loss-weight', '0.0001',
+    '--metrics', '',
+    '--metric', 'val_loss',
+    '--tasks', 'depth_visualization_task',
+    #'--evaluate',
+]
+sys.argv.extend(args)
+try:
+    main()
+except BdbQuit:
+    sys.exit(1)
+except Exception:
+    traceback.print_exc()
+    print('')
+    pdb.post_mortem()
+    sys.exit(1)
